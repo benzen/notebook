6727f361c34742768cbf969c445cb5e4dd60db62
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 13:10:45 -0400

    update on laod reaction

--- public/script/TeacherMarkingNotebook.js
+++ public/script/TeacherMarkingNotebook.js
@@ -3,6 +3,6 @@ TeacherMarkingNotebook.installListeners = function(){
   $(".saveClassButton").click(function(){console.log("click on save class buttonupdated" )});
   $(".addStudentButton").click(function(){console.log("click on add student button")});
 };
-$.ready(function(){
+$(document).ready(function(){
   TeacherMarkingNotebook.installListeners();
 });
