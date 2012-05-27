f05632ae216e7c64c91c5e63aa905e6b89a00c50
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 17:42:56 -0400

    add ability to save student

--- public/views/StudentEditView.js
+++ public/views/StudentEditView.js
@@ -3,19 +3,27 @@ $(document).ready(function(){
     el: $('body'),
     events: {
       "change [name='firstname']":   "studentChanged",
-      "change .lastname":    "studentChanged",
-      "change .birthday":    "studentChanged",
-      "change .notes":       "studentChanged",
-      "change .telephones":  "studentChanged",
-      "change .fatherName":  "studentChanged",
-      "change .motherName":  "studentChanged",
+      "change [name='lastname']":    "studentChanged",
+      "change [name='birthday']":    "studentChanged",
+      "change [name='notes']":       "studentChanged",
+      "change [name='telephones']":  "studentChanged",
+      "change [name='fatherName']":  "studentChanged",
+      "change [name='motherName']":  "studentChanged",
     },
     initialize: function(){
       _.bindAll(this, 'studentChanged');
       this.model = new Student()
     },
     studentChanged:function(e){
-       this.model.save({firstname:$(".firstname")});
+       this.model.save({
+         firstname:$("[name='firstname']").val(),
+         lastname:$("[name='lastname']").val(),
+         birthday:$("[name='birthday']").val(),
+         notes:$("[name='notes']").val(),
+         telephoneNumber:$("[name='telephoneNumber']").val(),
+         fatherName:$("[name='fatherName']").val(),
+         motherName:$("[name='motherName']").val(),
+       });
     }
   });
 
