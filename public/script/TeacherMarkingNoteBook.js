93d5bd11f777d17adc01a5473ee950573bf5d10c
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 13:05:01 -0400

    add reaction to buttons

--- public/script/TeacherMarkingNoteBook.js
+++ public/script/TeacherMarkingNoteBook.js
@@ -1,6 +1,6 @@
 TeacherMarkingNotebook={};
 TeacherMarkingNotebook.installListeners = function(){
-  $(".saveClassButton").click(function(){console.log("click on save class button")});
+  $(".saveClassButton").click(function(){console.log("click on save class buttonupdated" )});
   $(".addStudentButton").click(function(){console.log("click on add student button")});
 };
 $.ready(function(){
