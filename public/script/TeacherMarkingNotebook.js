062399d94e252d26fd6d0d0ac94b1a7d695b5269
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 13:19:20 -0400

    get student value:

--- public/script/TeacherMarkingNotebook.js
+++ public/script/TeacherMarkingNotebook.js
@@ -3,6 +3,18 @@ TeacherMarkingNotebook.installListeners = function(){
   $(".saveClassButton").click(function(){console.log("click on save class buttonupdated" )});
   $(".addStudentButton").click(function(){console.log("click on add student button")});
 };
+TeacherMarkingNotebook.saveStuddent = function(){
+  var student ={
+    firstname: $("[name='firstname']").val(),
+    lastname: $("[name='lastname']").val(),
+    birtday:$("[name='birtday']").val(),
+    fatherName: $("[name='fatherName']").val(),
+    motherName: $("[name='motherName']").val(),
+    telephones: $("[name='telephones']").val(),
+    notes: $("[name='notes']").val()
+  };
+  
+};
 $(document).ready(function(){
   TeacherMarkingNotebook.installListeners();
 });
