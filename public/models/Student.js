554db97cf0d68caf302b1dc3981dda373b83d2c4
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 15:14:33 -0400

    allow config student with mapwq

--- public/models/Student.js
+++ public/models/Student.js
@@ -1,9 +1,9 @@
-function Student(){
-  this.firstName = "";
-  this.lastName = "";
-  this.birthdate = new Date();
-  this.phoneNumber = [];
-  this.notes = ""
+function Student(s){
+  this.firstName = s?s.firstname:"";
+  this.lastName = s?s.lastname:"";
+  this.birthdate = s?s.birthday:new Date();
+  this.phoneNumber = s?s.phonenumber:[];
+  this.notes = s?s.notes""
 };
 Student.prototype.name = function(){
      return this.firstName+" "+this.lastName;
