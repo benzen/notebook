6bfc2da68d8ea8e14f30d07a2e83ec2c1016338e
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 13:51:54 -0400

    use models

--- public/script/TeacherMarkingNotebook.js
+++ public/script/TeacherMarkingNotebook.js
@@ -1,10 +1,19 @@
+
+
 TeacherMarkingNotebook={};
+TeacherMarkingNotebook.studentClass = new Class();
 TeacherMarkingNotebook.installListeners = function(){
-  $(".saveClassButton").click(function(){console.log("click on save class buttonupdated" )});
-  $(".addStudentButton").click(function(){console.log("click on add student button"); TeacherMarkingNotebook.saveStuddent();});
+  $(".saveClassButton").click(function(){
+    console.log("click on save class buttonupdated" );
+    TeacherMarkingNotebook.saveClass();
+  });
+  $(".addStudentButton").click(function(){
+    console.log("click on add student button");
+    TeacherMarkingNotebook.saveStuddent();
+  });
 };
 TeacherMarkingNotebook.saveStuddent = function(){
-  var student ={
+  var student = new Student({
     firstname: $("[name='firstname']").val(),
     lastname: $("[name='lastname']").val(),
     birtday:$("[name='birtday']").val(),
@@ -12,8 +21,16 @@ TeacherMarkingNotebook.saveStuddent = function(){
     motherName: $("[name='motherName']").val(),
     telephones: $("[name='telephones']").val(),
     notes: $("[name='notes']").val()
-  };
-  
+  });
+  TeacherMarkingNotebook.studentClass.students.push(student);
+};
+
+
+TeacherMarkingNotebook.saveClass = function(){
+  TeacherMarkingNotebook.studentClass.set({
+    group:$(".group").val(), 
+    year;$("year").val()
+  });
 };
 $(document).ready(function(){
   TeacherMarkingNotebook.installListeners();
