96d87285b03b50b9fdb81fc1115743ca0602399d
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-27 22:16:39 -0400

    rename class view

--- public/views/ClassCreateView.js
+++ public/views/ClassCreateView.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
-  StudentListView = Backbone.View.extend({    
+  ClassCreateView = Backbone.View.extend({    
     el: $('body'),
     initialize: function(){
-      _.bindAll(this, 'render', 'addStudent','appendStudent');
-      this.collection = new Students()
-      this.collection.bind('add', this.appendStudent);
+      _.bindAll(this, 'render', 'addStudent','appendStudent','clearForm');
+      this.model = new Class({students:new Students()});
+      this.model.get("students").bind('add', this.appendStudent);
       this.render();
     },
     events: {
-      'click .addStudentButton': 'addStudent'
+      'click .addStudentButton': 'addStudent',
+      'click .saveClassButton': 'saveClass'
+//, add ability to remove a student
     },
     render: function(){
       var self = this;
-      _(this.collection.models).each(function(student){ 
-        self.appendItem(item);
+      _(this.model.get("students")collection.models).each(function(student){ 
+        self.appendItem(student);
       }, this);
     },
     addStudent: function(){
@@ -26,8 +28,19 @@ $(document).ready(function(){
          fatherName:$("[name='fatherName']").val(),
          motherName:$("[name='motherName']").val()
       });
-      this.collection.add(s);
+      this.model.get("students").add(s);
+      this.clearForm();
     },
+   clearForm:function(){
+     $("[name='firstname']").val("");
+     $("[name='lastname']").val("");
+     $("[name='birthday']").val("");
+     $("[name='notes']").val("");
+     $("[name='telephone']").val("");
+     $("[name='fatherName']").val("");
+     $("[name='motherName']").val("");
+
+   }.
    appendStudent: function(student){
       $(".studentList table tbody",this.el).append( 
                   "<tr>  <td>"+ student.get("firstname") + "</td>"+
@@ -37,6 +50,12 @@ $(document).ready(function(){
                          "<td>"+ student.get("motherName") + "</td>"+
                          "<td>"+ student.get("telephone") + "</td>"+
                          "<td>"+ student.get("notes") + "</td> </tr>");
+    },
+   saveClass:function(){
+     var Class = {
+       group:$("[name='group']").val(),
+       year:$("[name='year']").val(),
+       students: this.collection.toJSON()
     }
   });
 
