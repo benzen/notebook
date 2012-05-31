0a0d858f06bbb9b4d82016e69410380f62ec77e9
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-31 18:47:27 -0400

    correct path to db

--- modules/Db.js
+++ modules/Db.js
@@ -2,7 +2,7 @@ var pg = require( 'pg' );
 var env = require("./Properties.js");
 
 
-var dbUrl = env.map.DB_URL + env.map.DB_NAME;
+var dbUrl = env.map.DB_URL + "/"+env.map.DB_NAME;
 
 var client = new pg.Client( dbUrl );
 client.connect();
