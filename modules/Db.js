538f99013397633540a15c25111f7dfb95e4f39d
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-31 19:00:03 -0400

    typo

--- modules/Db.js
+++ modules/Db.js
@@ -1,8 +1,8 @@
 var pg = require( 'pg' );
 var env = require("./Properties.js");
 
-var dbUrl = env.map.DOTCLOUD_DATA_SQL_URL +"/"+env.map.DB_NAME;
-//var dbUrl = env.map.DOTCLOUD_DATA_DB_URL + "/"+env.map.DB_NAME;
+//var dbUrl = env.map.DOTCLOUD_DATA_SQL_URL +"/"+env.map.DB_NAME;
+var dbUrl = env.map.DOTCLOUD_DATA_DB_URL + "/"+env.map.DB_NAME;
 
 var client = new pg.Client( dbUrl );
 client.connect();
