fa07f77ac6140554fecda0b57e9b0938b4e87c4b
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-05-31 19:38:07 -0400

    add schema creation

--- modules/Db.js
+++ modules/Db.js
@@ -7,4 +7,8 @@ var dbUrl = env.map.DB_URL +"/"+env.map.DB_NAME;
 var client = new pg.Client( dbUrl );
 client.connect();
 
+var createTables  = function(){
+  client.query("CREATE TABLE STUDENT ( id SERIAL PRIMARY KEY, json text)");
+};
+createTables();
 exports.db = client;
