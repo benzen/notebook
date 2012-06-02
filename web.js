f448e9a7b6d884700f12a50f7ee36d8ac8c8bca2
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-02 16:55:25 -0400

    add translation mecanism

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require( 'express' );
 var classController = require( "./controllers/Class.js" );
 var navigationController = require( "./controllers/Navigation.js" );
-
+var i18next = require("i18next");
+i18next.init();
 
 var publicDir = __dirname +"/public";
 var app = express.createServer(express.logger());
@@ -9,6 +10,8 @@ app.use(express.static( publicDir ));
 app.set('view engine', 'jade');
 app.set('view options', { layout: false });
 app.use(express.bodyParser());
+app.use(i18next.handle);
+i18next.registerAppHelper(app);
 
 app.get('/', navigationController.index );
 
