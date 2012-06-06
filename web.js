192df322374dd6dd0285facf3544ced2bfa8877b
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-06 19:55:32 -0400

    remove authentication

--- web.js
+++ web.js
@@ -2,18 +2,6 @@ var express = require( 'express' );
 var classController = require( "./controllers/Class.js" );
 var navigationController = require( "./controllers/Navigation.js" );
 var i18next = require("i18next");
-var passport = require("passport-openid");
-
-passport.use(new GoogleStrategy({
-    returnURL: 'http://localhost:3000/auth/google/return',
-    realm: 'http://localhost:3000/'
-  },
-  function(identifier, done) {
-    User.findByOpenID({ openId: identifier }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
 
 18next.init();
 var publicDir = __dirname +"/public";
@@ -35,20 +23,6 @@ app.get("/class/:id", classController.showClass );
 app.get("/class/:id/edit", classController.editClass );
 app.put("/class/:id", classController.updateClass );
 
-
-app.get('/auth/google',
-  passport.authenticate('google'),
-  function(req, res){
-    // The request will be redirected to Google for authentication, so
-    // this function will not be called.
-  });
-
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
 
 var port = 8080;
 app.listen(port, function() {
