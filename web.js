a1bc6854960465cc4130d5e92ce0cf43a9c25cab
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-07 19:47:26 -0400

    try with twitter

--- web.js
+++ web.js
@@ -2,7 +2,10 @@ var express = require( 'express' );
 var classController = require( "./controllers/Class.js" );
 var navigationController = require( "./controllers/Navigation.js" );
 var i18next = require("i18next");
+var auth = require('connect-auth');
 
+var twitterConsumerKey = "tjcaHSMkGUItYqEvyyRAA";
+var twitterSecret = "DELa45PgFNJzaCbNSVv5XckRtstJorqolgV6UwMIHok";
 i18next.init();
 var publicDir = __dirname +"/public";
 var app = express.createServer(express.logger());
@@ -10,6 +13,9 @@ app.use(express.static( publicDir ));
 app.set('view engine', 'jade');
 app.set('view options', { layout: false });
 app.use(express.bodyParser());
+app.use(auth( [
+   auth.Twitter({consumerKey: yourTwitterConsumerKey, consumerSecret: yourTwitterConsumerSecret})
+  ]) );
 app.use(i18next.handle);
 i18next.registerAppHelper(app);
 
@@ -23,7 +29,25 @@ app.get("/class/:id", classController.showClass );
 app.get("/class/:id/edit", classController.editClass );
 app.put("/class/:id", classController.updateClass );
 
+app.get('/secrets', protect, function(req, res){
+    res.send('Shhhh!!! Unicorns');
+});
 
+function protect(req, res, next) {
+  if( req.isAuthenticated() ) next();
+  else {
+    req.authenticate(function(error, authenticated) {
+      if( error ) next(new Error("Problem authenticating"));
+      else {
+        if( authenticated === true)next();
+        else if( authenticated === false ) next(new Error("Access Denied!"));
+        else {
+          // Abort processing, browser interaction was required (and has happened/is happening)
+        }
+      }
+    })
+  }
+}
 var port = 8080;
 app.listen(port, function() {
   console.log("I will stay tuned on " + port);
