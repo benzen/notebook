12ff4fa5dcc464d6c74a4c6c32b4f7de858a9b20
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 18:35:27 -0400

    new way to check user existance

--- modules/Users.js
+++ modules/Users.js
@@ -2,18 +2,21 @@ var db = require("./Db.js").db;
 
 exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
   var query = db.query("SELECT name from \"user\" where type = $1 and id = $2", ['twitter', twitterData.id_str]);
+  var userNeedToBeCreated = true;
   //user exist case
   query.on("row", function( row ){
     console.log("row"+ JSON.stringify(row));
     var user = {type:'twitter', id :twitterData.id_str, name:row.name};
     promise.fulfill(user);
+    userNeedToBecreated = false;
   });
   //user doesn't exist yet case
-  query.on('error',function(){
-    promise.error("no result");
-    db.query("INSERT into \"user\" (type, id, name) VALUES ( $1,$2,$3)", ['twitter', twitterData.id_str, twitterData.screen_name]);
-    var user = {type:'twitter', id :twitterData.id_str, name:twitterData.screen_name};
-    promise.fulfill(user);
+  query.on('end',function(){
+    if(userNeedToBeCreated){
+      db.query("INSERT into \"user\" (type, id, name) VALUES ( $1,$2,$3)", ['twitter', twitterData.id_str, twitterData.screen_name]);
+      var user = {type:'twitter', id :twitterData.id_str, name:twitterData.screen_name};
+      promise.fulfill(user);
+    }
   });
 
 //  var classAsJson = JSON.stringify( request.body );
