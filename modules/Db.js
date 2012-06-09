8cd2c84781eccb84fc8fcb4b60012fbccb1880c8
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 11:48:32 -0400

    add log message for each table

--- modules/Db.js
+++ modules/Db.js
@@ -7,10 +7,14 @@ var dbUrl = env.map.DB_URL +"/"+env.map.DB_NAME;
 var client = new pg.Client( dbUrl );
 client.connect();
 
-var query = client.query("CREATE TABLE class ( id SERIAL PRIMARY KEY, json text)");
-query = client.query("CREATE TABLE user (type text, id text, name text)")
-query.on("error", function(){
+var ifTableAlreadyExistLogAMessage = function(){
   console.log("ERROR tables already exist")
-});
+};
+
+var query = client.query("CREATE TABLE class ( id SERIAL PRIMARY KEY, json text)");
+query.on("error", ifTableAlreadyExistLogAMessage);
+
+query = client.query("CREATE TABLE user (type text, id text, name text)")
+query.on("error",ifTableAlreadyExistLogAMessage);
 
 exports.db = client;
