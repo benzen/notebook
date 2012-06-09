ff4c64afd89af8d40a2256f074df8391ab5e56ca
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 17:54:39 -0400

    change error message when table already exist

--- modules/Db.js
+++ modules/Db.js
@@ -7,14 +7,14 @@ var dbUrl = env.map.DB_URL +"/"+env.map.DB_NAME;
 var client = new pg.Client( dbUrl );
 client.connect();
 
-var ifTableAlreadyExistLogAMessage = function(){
-  console.log("ERROR tables already exist")
+var ifTableAlreadyExistLogAMessage = function(tableName){
+  return function(){ console.log("ERROR table " + tableName + " already exist")});
 };
 
 var query = client.query("CREATE TABLE class ( id SERIAL PRIMARY KEY, json text)");
-query.on("error", ifTableAlreadyExistLogAMessage);
+query.on("error", ifTableAlreadyExistLogAMessage("class"));
 
 query = client.query("CREATE TABLE user (type text, id text, name text)");
-query.on("error",ifTableAlreadyExistLogAMessage);
+query.on("error",ifTableAlreadyExistLogAMessage("user"));
 
 exports.db = client;
