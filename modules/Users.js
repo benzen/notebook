5ae7e8d9aa7afa57b93bb6e9759a91976f1afc94
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 17:43:00 -0400

    typo

--- modules/Users.js
+++ modules/Users.js
@@ -1,6 +1,7 @@
-var db = require("../modules/Db.js").db;
+var db = require("./Db.js").db;
 
 exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
+  console.log("find or create a user");
   var query = db.query("SELECT name from user where type = $1 and id = $2", ['twitter', twitterData.id_str]);
   query.on("row", function( row ){
     console.log("row"+ JSON.stringify(row));
