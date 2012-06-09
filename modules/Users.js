59ec234668853cfc87a185fb40223d564d81ab0d
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 18:21:43 -0400

    add user creation

--- modules/Users.js
+++ modules/Users.js
@@ -1,8 +1,7 @@
 var db = require("./Db.js").db;
 
 exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
-  console.log("find or create a user");
-  var query = db.query("SELECT name from 'user' where type = $1 and id = $2", ['twitter', twitterData.id_str]);
+  var query = db.query("SELECT name from \"user\" where type = $1 and id = $2", ['twitter', twitterData.id_str]);
   //user exist case
   query.on("row", function( row ){
     console.log("row"+ JSON.stringify(row));
@@ -11,8 +10,9 @@ exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
   });
   //user doesn't exist yet case
   query.on('error',function(){
-    console.log("there is no result");
-    promise.fail("no result");
+    db.query("INSERT into \"user\" (type, id, name) VALUES ( $1,$2,$3)", ['twitter', twitterData.id_str, twitterData.screen_name]);
+    var user = {type:'twitter', id :twitterData.id_str, name:twitterData.screen_name};
+    promise.fulfill(user);
   });
 
 //  var classAsJson = JSON.stringify( request.body );
