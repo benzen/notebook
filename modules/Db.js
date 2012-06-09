c7a63cf045f26d68b02509d263f608e651ed5cfe
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-09 18:12:46 -0400

    use user db name and check for error

--- modules/Db.js
+++ modules/Db.js
@@ -14,7 +14,7 @@ var ifTableAlreadyExistLogAMessage = function(tableName){
 var query = client.query("CREATE TABLE class ( id SERIAL PRIMARY KEY, json text);");
 query.on("error", ifTableAlreadyExistLogAMessage("class"));
 
-query = client.query("CREATE TABLE users (type text, id text, name text);");
+query = client.query("CREATE TABLE 'user' (type text, id text, name text);");
 query.on("error",ifTableAlreadyExistLogAMessage("user"));
 
 exports.db = client;
--- modules/Users.js
+++ modules/Users.js
@@ -2,11 +2,17 @@ var db = require("./Db.js").db;
 
 exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
   console.log("find or create a user");
-  var query = db.query("SELECT name from users where type = $1 and id = $2", ['twitter', twitterData.id_str]);
+  var query = db.query("SELECT name from 'user' where type = $1 and id = $2", ['twitter', twitterData.id_str]);
+  //user exist case
   query.on("row", function( row ){
     console.log("row"+ JSON.stringify(row));
     var user = {type:'twitter', id :twitterData.id_str, name:row.name};
     promise.fulfill(user);
+  });
+  //user doesn't exist yet case
+  query.on('error',function(){
+    console.log("there is no result");
+    promise.fail("no result");
   });
 
 //  var classAsJson = JSON.stringify( request.body );
