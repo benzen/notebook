0b2b4f13f5892ebde19cee35d0f6d2c2c6a3f0d3
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-06-15 17:40:09 -0400

    remove debug messages

--- modules/Users.js
+++ modules/Users.js
@@ -4,26 +4,17 @@ exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
   var query = db.query("SELECT name from \"user\" where type = $1 and id = $2", ['twitter', twitterData.id_str]);
   //user exist case
   query.on("row", function( row ){
-    console.log("row"+ JSON.stringify(row));
     var user = {type:'twitter', id :twitterData.id_str, name:row.name};
     promise.fulfill(user);
   });
   //user doesn't exist yet case
   query.on('end',function(result){
     if(result.rowCount === 0 ){
-      console.log(result.rowCount);
-      console.log("HEY YOU NEED A USER");
       db.query("INSERT into \"user\" (type, id, name) VALUES ( $1,$2,$3)", ['twitter', twitterData.id_str, twitterData.screen_name]);
       var user = {type:'twitter', id :twitterData.id_str, name:twitterData.screen_name};
       promise.fulfill(user);
     }
   });
-
-//  var classAsJson = JSON.stringify( request.body );
-//  var query = db.db.query("INSERT INTO class ( json ) values( $1 ) RETURNING id", [ classAsJson ]);
-//  query.on("row", function(row){
-//    response.send("/class/"+row.id);
-//  });
 
 };
 
