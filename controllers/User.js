c647fa2d5a514389ec17fe25e622fdd70cde9659
Benjamin Dreux <dreux.benjamin@uqam.ca>
2012-09-30 02:28:01 -0400

    remove dep to underscore

--- controllers/User.js
+++ controllers/User.js
@@ -1,5 +1,4 @@
-var db = require("../modules/Db.js").db,
-    _  = require("underscore");
+var db = require("../modules/Db.js").db;
 
 exports.findOrCreateUserByTwitterData  = function(promise, twitterData){
   var query = db.query("SELECT name from \"user\" where auth_type = $1 and auth_id = $2", ['twitter', twitterData.id_str]);
@@ -28,7 +27,6 @@ exports.updateProfile = function(request, response){
 
 };
 exports.getProfile = function( request, response ){
-//  console.log( JSON.stringify( request.session.auth.twitter.user.id ) );
   var id = request.session.auth.twitter.user.id;
   var query = db.query("select profile from \"user\" where auth_id=$1",[ id ] );
   query.on( "row", function( row ){
@@ -53,28 +51,3 @@ exports.findUserById=function(userId, callback){
     callback(error);
   });
 };
-
-/*
-exports.partialProfileUpdate = function( userId, profileUpdate, response ){
-  getProfle(userId,function(profile){
-    var newProfile = _.extend( profile, profileUpdate );
-    updateProfile(userId,newProfile, function(updatedProfile){
-      response.render("user/profile", { profile: updatedProfile } );
-    });
-      
-  });
-};
-*/
-/*
-exports.addClassToProfile=function(request,response){
-  var userId = request.params.userId;
-  var classId = request.params.classId;
-  getProfile(userId, function( profile ){
-    profile.classList = profile.class?profile.classList:[];
-    profile.classList.push( classId );
-    User.partialProfileUpdate( userId, profile, function( profileUpdate ){
-      response.render( "user/profile", {profile:profileUpdate});
-    });  
-  });
-};
-*/
